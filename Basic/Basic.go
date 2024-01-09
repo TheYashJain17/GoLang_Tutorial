60165474d8cd12e3c519a82799b42f9891d32396
yash jain <yashjain07.yj@gmail.com>
2024-01-10 01:38:08 +0530

    Created Our First Go Program.

---
+++ Basic/Basic.go
@@ -0,0 +1,27 @@
+/*The order we have provided below should remain same means first we have to declare the package which is main
+in this case as it helps our function to run and then we have to import the "fmt" package to use it
+and then at last we can defined the function to use it and remember to keep the function name main means
+same as package name.*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("GoodBye World") //use println instead of print command to get the better result.
+
+	//fmt.Println is just like the console.log of javascript , just gets used to print something in the terminal.
+
+}
+
+/*For running this just simply go in the terminal and then go to the desired directory like in our case
+it is basic directory so type :- cd basic then when you are inside that directory type:-
+go run then filename with go extension which will gonna see like this :- go run basic.go and after 
+thats it this will gonna run.*/
+
+//And thats how you can write your first program in go lanuage.
+
+// This was our first Program in go language.
+
+//Stay tunes for more updates.
