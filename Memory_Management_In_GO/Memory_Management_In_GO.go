8463237a3199c37223641f9957a185089c40153a
yash jain <yashjain07.yj@gmail.com>
2024-01-17 20:50:09 +0530

    Completed Memory Management Session In Go.

---
+++ Memory_Management_In_GO/Memory_Management_In_GO.go
@@ -0,0 +1,37 @@
+// In this session we gonna see that how does memory management actually happens in go.
+
+// This will gonna help us understand that what method should we use at a certain place.
+
+/*In go language memory management(allocation and deallocation) happens automatically.
+But lets understand little theory about regarding the same.*/
+
+// There are 2 methods through which memory management happens:-
+
+// 1. new()
+
+// Lets first understand the new() method.
+
+// In case of new method , memory is allocated but never initialised.
+
+// We will get a memory address so that we can refer it and get the location.
+
+// 2. make()
+
+// In case of new method , we get zeroed storage means we cannot put any data initially.
+
+// Now lets understand the make method.
+
+// In case of make method , memory is allocated and also initialised.
+
+// We will get a memory address so that we can refer it and get the location.
+
+// In case of make method , we get non zeroed storage which means we can put any data initially.
+
+// MOSTLY WE GONNA USE THE make METHOD.
+
+// We will understand more about this through practical.
+
+/*So this was all about this session , it is important as we gonna keep this in mind while working 
+and using methods.*/
+
+// Stay tuned for more updates.
