2cd2a580888fa42c2721c925d0a05078ba9202bf
yash jain <yashjain07.yj@gmail.com>
2024-03-13 00:11:39 +0530

    In This Session We Have Learnt About The GET Request.

--- BuildingAPI/BuildingAPI.go
+++ BuildingAPI/BuildingAPI.go
@@ -7,6 +7,15 @@ so that you can understand it better.*/
 
 package main
 
+import(
+
+	"encoding/json"
+	"net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+
+)
+
 func main(){
 
 
@@ -22,7 +31,7 @@ it means that what will the user as a response in the form of json thats it , no
 type Course struct{
 
 	CourseId string `json:"id"`
-	CoureName string `json:"name"`
+	CourseName string `json:"name"`
 	CoursePrice int `json:"price"`
 	CourseAuthor Author `json:"author"`//made this of Author type which is the struct we made below.
 	Tags []string `json:"tags,omitempty"`
@@ -64,11 +73,154 @@ And if you have taken my methods tutorial then you must know that how do we use 
 */
 
 
-func (_course *Course) checkIfEmpty(){
+func (_course *Course) checkIfEmpty() bool {
 
 	return _course.CourseName == "";
 
 }
 
-// stay tuned for more updates.
+
+/*
+In our above session we saw kind of models , so now in this session we gonna see controllers.
+
+You can understand controllers as fucntions which gets executed whenever we make a request to the route.
+
+You can understand this if you have seen my sample server tutorial , so now lets see a example of it.
+
+Usually controllers are also made in different file but here we are keeping this in the same file to keep
+the things simple.
+
+*/
+
+/*
+So created a function can say controller to the control the homepage route so whenver someone
+will visit to our homepage he/she will see this response , i have discussed about this in detail
+in sample server tutorial so kindly check it for more details.
+*/
+
+func renderingHomePage(w http.ResponseWriter , r *http.Request){
+
+	return w.Write([]byte("<h1>This is Our Homepage</h1>"))
+
+
+}
+
+/*
+So this is our another controller which gonnna handle the route whenver someone wanna see all the
+courses we have.
+
+So to do this we are making this function or can say controller.
+
+So again taking the paramters as it is required to do so , check sample server for more details.
+
+Therefore doing so and inside the funciton  we are first printing to tell what we are doing with
+this controller.
+
+After that we have to set headers , if you know about something backend then you must know about headers
+And if you dont know that what does they do let me tell you in easy words ,
+
+So basically till now we have understood that we deal data in json for, in backend , so by setting header
+we are saying that we are setting the content-type to json , it should be written in the same way  as i have
+written below else you will get error , so basically by doing this we are telling that the type of data will
+gonna be of json.
+
+after setting the header we are sending the response in the form of json with the help of json library.
+
+So with the help of len method we are checking of the length of courses slice that whether it is greater than 0 or not
+
+if the length is greater than 0 then we are sending the response.
+
+so for sending response in json we have to use NewEncoder property of json and inside that we have to provide
+a writer to write the response which is w in this case what we have taken in paramter this will gonna do the
+job of writing the response for us and then we have to use the Encode property to send the data and inside
+this we are passing the slice of Course struct we declare above.
+
+So thats how user can get all the courses as gonna store all the courses in this slice.
+
+else there will be a response saying there are no courses to display.
+
+for starting there are no courses , but after sometime we will gonna add some data manually , this process
+is known as data seeding.
+
+dont worry if you are finding this little complicated , its absolutely fine to feel so , its just about the
+understanding the syntax, you  will get familiar with the syntax soon dont worry just code along with me.
+*/
+
+func getAllCourses(w http.ResponseWriter , r *http.Request){
+
+	fmt.Println("Getting All Courses")
+
+	w.Header().Set("Content-Type" , "application/json");
+
+	if len(courses) > 0 {
+
+		json.NewEncoder(w).Encode(courses);
+
+
+	}else{
+
+		json.NewEncoder(w).Encode("There are no courses to display")
+
+	}
+
+}	
+
+
+/*
+
+Now this is our controller for getting a single course based on the id the user is providing.
+
+In case of these controllers most of the process will gonna remain same like taking parameters
+like http.ResponseWriter and Request then inside the controller/function we have to set the content-type
+which is application/json so that we can send in the form of json and user can get the response.
+
+Now in this controlller the main thing is getting the id from the user because on the basis of that id
+we gonna find the course , the user is looking for.
+
+so for getting the id which the user is providing we are using mux which is gorilla mux which we installed
+and using .Vars property of it and inside providing the r which is handling the user request.
+
+Now we got all the things in the form of key value pairs inside params variable.
+
+Now we are looping through all the courses with the help of for loop using range keyword to match
+the id with the user is providing with the actual course id to see whether there is such id or not.
+
+if there is one then we are sending that in the form of json with the help of json library and its Property
+of NewEncoder and writing with the w variable which is our ResponseWriter then using .Encode to send the course
+we found in the form of json, then atlast using return keyword to say that task is completed now return
+its upto use whether you want to use it or not as it gonna work same ways.
+
+and atlast after exiting the loop we are sending response in json saying that there is no course with this id.
+
+and atlast again using return to say that process is completed and now return again its upto you whether you
+want to use it or not as it gonna work same ways.
+
+*/
+
+func getOneCourse(w http.ResponseWriter , r *http.Request){
+
+	fmt.Println("Getting Single Course");
+
+	w.Header().Set("Content-Type" , "application/json");
+
+	params := mux.Vars(r);
+
+	for _ , course := range courses{
+
+		if course.CourseId == params["id"]{
+
+			json.NewEncoder(w).Encode(course);
+
+		}else{
+
+			json.NewEncoder(w).Encode("Please Enter A Valid Id");
+
+		}
+
+	}
+
+
+}	
+
+//stay tuned for more updates
 
